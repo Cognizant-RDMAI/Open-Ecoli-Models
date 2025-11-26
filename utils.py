2c804bbb95756f7c0001b61cce6000ffd13d4918
Ashish T <ashish.tiwari5@cognizant.com>
2025-11-26 18:40:17 +0000

    V2 of the Open E.coli Models - release/V2

--- utils.py
+++ utils.py
@@ -1,5 +1,31 @@
 import math
+from datetime import datetime, timedelta
 
+# convert date to required format ayyyyddd for MODIS API calls
+def convert_date_to_ayyyyddd(date_str, n_days1, n_days2): 
+    # Parse the input date string
+    date_obj = datetime.strptime(date_str, "%Y-%m-%d") - timedelta(days=n_days1)
+    
+    # Add n days to the date
+    new_date_obj = datetime.strptime(date_str, "%Y-%m-%d") + timedelta(days=n_days2)
+
+    # Get the year and day of year
+    year1 = date_obj.year
+    day_of_year1 = date_obj.strftime('%j')  # Julian day
+    
+    # Get the year and day of year
+    year2 = new_date_obj.year
+    day_of_year2 = new_date_obj.strftime('%j')  # Julian day
+    
+    # Format the result
+    a_year1 = f"A{year1}"
+    a_year_day1 = f"{a_year1}{day_of_year1}"
+
+    # Format the result
+    a_year2 = f"A{year2}"
+    a_year_day2 = f"{a_year2}{day_of_year2}"
+    
+    return a_year_day1, a_year_day2
 
 
 # Convert distance to radians (since 1 radian = Earth's radius)
